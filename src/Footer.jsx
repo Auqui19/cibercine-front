a1d35e8165fcde260ce64c863ad595207d8da33f
Alonso Auqui <andriod.pvp@gmail.com>
2024-09-28 20:48:44 -0500

    🧦 Footer

---
+++ src/Footer.jsx
@@ -0,0 +1,38 @@
+function Footer() {
+  return (
+    <div className="w-full bg-[#020305] flex justify-center"> 
+        <div className="flex justify-center items-center w-11/12">
+            <div className="flex justify-center items-center">
+                <div className="flex flex-col items-center">
+                    <div className="text-center">
+                        <h1 className="text-2xl font-bold text-white">
+                            cibercine
+                        </h1>
+                        <p className="text-center text-white">
+                            cibercine is a social networking platform that
+                            allows users to create profiles, connect with
+                            friends, and share their interests and hobbies.
+                        </p>
+                    </div>
+                </div>
+            </div> 
+            <div className="flex justify-center items-center">
+                <div className="flex flex-col items-center">
+                    <div className="text-center">
+                        <h1 className="text-2xl font-bold text-white">
+                            cibercine
+                        </h1>
+                        <p className="text-center text-white">
+                            cibercine is a social networking platform that
+                            allows users to create profiles, connect with
+                            friends, and share their interests and hobbies.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
